bb2978f1156cd9d203ef5b8b4b7c10e5974123a7
Elderick <erickoliveiraah@gmail.com>
2019-10-04 23:08:00 -0300

    initial commit
    Base da aplicação

---
+++ base/data.js
@@ -0,0 +1,14 @@
+window.audios = [
+   {
+      title: "Arrasta",
+      artist: "Glória Groove (Feat. Léo Santana)",
+      cover:  "arrasta-gloria-groove.jpg",
+      file: "arrasta-gloria-groove.mp3",
+   },
+   {
+      title: "Solta a Batida",
+      artist: "Ludmilla",
+      cover:  "solta-a-batida-ludmilla.jpg",
+      file: "solta-a-batida-ludmilla.mp3",
+   }
+];
